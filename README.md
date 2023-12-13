eead9aae3f41d7648f167ba80ed93365a2e27b95
Cameron Tofer <camtofer@gmail.com>
2023-12-13 15:53:45 -0700

    threw out luajit test.  threw in chat over ipfs

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 
 1. go generate
 2. go run fulcrum.go
+
+### Roadmap
--- fulcrum.go
+++ fulcrum.go
@@ -1,113 +1,184 @@
 package main
 
 import (
-	_ "embed"
+	"bufio"
+	"context"
+	"flag"
 	"fmt"
 	"os"
+	"sync"
+
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/protocol"
+	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
+	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
+
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	"github.com/multiformats/go-multiaddr"
+
+	"github.com/ipfs/go-log/v2"
 )
 
-//go:generate git submodule update --init --recursive
-//!!git checkout 5f6e0c0dff1e7a89331e6b25eca9a9fd71324069
-//go:generate mkdir -p llama.cpp/build
-//go:generate cmake -DLLAMA_STATIC=Off -DBUILD_SHARED_LIBS=ON -S llama.cpp -B llama.cpp/build
-//go:generate cmake --build llama.cpp/build --config Release
-//go:generate cp llama.cpp/build/libllama.dylib .
-//go:generate cp llama.cpp/ggml-metal.metal .
-//go:generate make -C LuaJIT -j16 BUILDMODE=static MACOSX_DEPLOYMENT_TARGET=14.1
-//go:generate make -C LuaJIT install PREFIX=$(PWD)/deps/luajit
+var logger = log.Logger("rendezvous")
 
-/*
-#cgo CFLAGS: -Ideps/luajit/include/luajit-2.1
-#cgo LDFLAGS: -Ldeps/luajit/lib -lluajit-5.1 -ldl -lm
-#include <luajit.h>
-#include <lua.h>
-#include <lauxlib.h>
-#include <lualib.h>
+func handleStream(stream network.Stream) {
+	logger.Info("Got a new stream!")
 
-#include "llama.cpp/llama.h"
-llama_token llama_sample_logits(float * logits, int n ) {
-    float best = -1e20;
-    llama_token best_id = 0;
-    for (int i=0; i<n; i++) {
-        if (logits[i] > best) {
-            best = logits[i];
-            best_id = i;
-        }
-    }
-    return best_id;
+	// Create a buffer stream for non-blocking read and write.
+	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+
+	go readData(rw)
+	go writeData(rw)
+
+	// 'stream' will stay open until you close it (or the other side closes it).
 }
-*/
-import "C"
 
-//go:embed llama.lua
-var llamadotlua []byte
-
-func servellm(L *C.lua_State, filename string) func(string) string {
-
-	// load llama.lua
-	if 0 != C.luaL_loadbuffer(L, (*C.char)(C.CBytes(llamadotlua)), C.size_t(len(llamadotlua)), C.CString("llama.lua")) {
-		panic(C.GoString(C.lua_tolstring(L, -1, nil)))
-	}
-
-	if C.lua_pcall(L, 0, -1, 0) != 0 {
-		err := C.lua_tolstring(L, -1, nil)
-		C.lua_settop(L, 0)
-		fmt.Println("error running function `f': ", C.GoString(err))
-	}
-
-	// start the llm server
-	C.lua_getfield(L, C.LUA_GLOBALSINDEX, C.CString("serve"))
-	C.lua_pushstring(L, C.CString(filename))
-	if C.lua_resume(L, 1) != C.LUA_YIELD {
-		fmt.Println("error on lua_resume", C.GoString(C.lua_tolstring(L, -1, nil)))
-		panic("llm server failed to start")
-	}
-
-	c := make(chan string)
-	go func() {
-		for prompt := range c {
-
-			fmt.Println("prompt:", prompt)
-
-			// continue the llm server with the prompt
-			C.lua_pushstring(L, C.CString(prompt))
-			r := C.lua_resume(L, 1)
-
-			switch r {
-			case C.LUA_OK:
-				fmt.Println("ok")
-			case C.LUA_YIELD:
-				answer := C.GoString(C.lua_tolstring(L, -1, nil))
-				C.lua_settop(L, 0)
-				c <- answer
-			default:
-				fmt.Println("error on lua_resume:", r, C.GoString(C.lua_tolstring(L, -1, nil)))
-			}
+func readData(rw *bufio.ReadWriter) {
+	for {
+		str, err := rw.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from buffer")
+			panic(err)
 		}
-	}()
 
-	return func(prompt string) string {
-		c <- prompt
-		return <-c
+		if str == "" {
+			return
+		}
+		if str != "\n" {
+			// Green console colour: 	\x1b[32m
+			// Reset console colour: 	\x1b[0m
+			fmt.Printf("\x1b[32m%s\x1b[0m> ", str)
+		}
+
+	}
+}
+
+func writeData(rw *bufio.ReadWriter) {
+	stdReader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		sendData, err := stdReader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from stdin")
+			panic(err)
+		}
+
+		_, err = rw.WriteString(fmt.Sprintf("%s\n", sendData))
+		if err != nil {
+			fmt.Println("Error writing to buffer")
+			panic(err)
+		}
+		err = rw.Flush()
+		if err != nil {
+			fmt.Println("Error flushing buffer")
+			panic(err)
+		}
 	}
 }
 
 func main() {
-
-	L := C.luaL_newstate()
-	defer C.lua_close(L)
-	C.luaL_openlibs(L)
-
-	filename := "/Users/cameron/.ollama/models/blobs/sha256:6504ba23a37160de70db611212815c9aab171864d206b8c013b72fd0b16e19eb"
-
-	if len(os.Args) > 1 {
-		filename = os.Args[1]
+	log.SetAllLoggers(log.LevelWarn)
+	log.SetLogLevel("rendezvous", "info")
+	help := flag.Bool("h", false, "Display Help")
+	config, err := ParseFlags()
+	if err != nil {
+		panic(err)
 	}
 
-	chat := servellm(L, filename)
+	if *help {
+		fmt.Println("This program demonstrates a simple p2p chat application using libp2p")
+		fmt.Println()
+		fmt.Println("Usage: Run './chat in two different terminals. Let them connect to the bootstrap nodes, announce themselves and connect to the peers")
+		flag.PrintDefaults()
+		return
+	}
 
-	fmt.Println("How do you cook an egg?\n", chat("How do you cook an egg?"), "===")
+	// libp2p.New constructs a new libp2p Host. Other options can be added
+	// here.
+	host, err := libp2p.New(libp2p.ListenAddrs([]multiaddr.Multiaddr(config.ListenAddresses)...))
+	if err != nil {
+		panic(err)
+	}
+	logger.Info("Host created. We are:", host.ID())
+	logger.Info(host.Addrs())
 
-	//fmt.Println("What do trains ride on?\n", chat("What do trains ride on?"), "===")
+	// Set a function as stream handler. This function is called when a peer
+	// initiates a connection and starts a stream with this peer.
+	host.SetStreamHandler(protocol.ID(config.ProtocolID), handleStream)
 
+	// Start a DHT, for use in peer discovery. We can't just make a new DHT
+	// client because we want each peer to maintain its own local copy of the
+	// DHT, so that the bootstrapping node of the DHT can go down without
+	// inhibiting future peer discovery.
+	ctx := context.Background()
+	kademliaDHT, err := dht.New(ctx, host)
+	if err != nil {
+		panic(err)
+	}
+
+	// Bootstrap the DHT. In the default configuration, this spawns a Background
+	// thread that will refresh the peer table every five minutes.
+	logger.Debug("Bootstrapping the DHT")
+	if err = kademliaDHT.Bootstrap(ctx); err != nil {
+		panic(err)
+	}
+
+	// Let's connect to the bootstrap nodes first. They will tell us about the
+	// other nodes in the network.
+	var wg sync.WaitGroup
+	for _, peerAddr := range config.BootstrapPeers {
+		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := host.Connect(ctx, *peerinfo); err != nil {
+				logger.Warning(err)
+			} else {
+				logger.Info("Connection established with bootstrap node:", *peerinfo)
+			}
+		}()
+	}
+	wg.Wait()
+
+	// We use a rendezvous point "meet me here" to announce our location.
+	// This is like telling your friends to meet you at the Eiffel Tower.
+	logger.Info("Announcing ourselves...")
+	routingDiscovery := drouting.NewRoutingDiscovery(kademliaDHT)
+	dutil.Advertise(ctx, routingDiscovery, config.RendezvousString)
+	logger.Debug("Successfully announced!")
+
+	// Now, look for others who have announced
+	// This is like your friend telling you the location to meet you.
+	logger.Debug("Searching for other peers...")
+	peerChan, err := routingDiscovery.FindPeers(ctx, config.RendezvousString)
+	if err != nil {
+		panic(err)
+	}
+
+	for peer := range peerChan {
+		if peer.ID == host.ID() {
+			continue
+		}
+		logger.Debug("Found peer:", peer)
+
+		logger.Debug("Connecting to:", peer)
+		stream, err := host.NewStream(ctx, peer.ID, protocol.ID(config.ProtocolID))
+
+		if err != nil {
+			logger.Warning("Connection failed:", err)
+			continue
+		} else {
+			rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+
+			go writeData(rw)
+			go readData(rw)
+		}
+
+		logger.Info("Connected to:", peer)
+	}
+
+	select {}
 }
